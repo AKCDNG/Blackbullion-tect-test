8ff41e01b99dcdd81b19489d352e128fec66fdd8
Anish <anishkakaiya@outlook.com>
2024-03-01 16:16:46 +0000

    add path component to render path blocks

--- frontend/src/path.tsx
+++ frontend/src/path.tsx
@@ -1,7 +1,12 @@
-function Path({ title }: { title: string }) {
+import type PathType from '../lib/constants';
+import './path.scss';
+
+function Path({ title, image, url }: PathType) {
   return (
-    <div className='path-wrap'>
-      <h1>{title}</h1>
+    <div className='path-wrap-inner'>
+      <img src={image} alt='' />
+      <h2>{title}</h2>
+      <a href={url}>View Pathway →</a>
     </div>
   );
 }
