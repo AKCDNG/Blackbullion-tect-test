1bc43fb7172ebc8d429bb6e4ee55a74d1db78014
Anish <anishkakaiya@outlook.com>
2024-03-01 15:53:29 +0000

    add path component to render out paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,46 @@
 import { useState, useEffect } from 'react';
+import Path from './path';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+type Path = {
+  id: number;
+  title: string;
+  internal_title: string;
+  url: string;
+  intro: string;
+  duration: string;
+  image: string;
+  type: string;
+  has_summative_assessment: boolean;
+};
 const URL: string = 'https://www.blackbullion.com/api/_dev/pathways';
 
 function App() {
   const controller = new AbortController();
-  const [apiFetch, setApiFetch] = useState(0);
+  const [apiFetch, setApiFetch] = useState<Array<Path>>([]);
 
   useEffect(() => {
-    async function getData() {
-      const response = await fetch(URL);
-      const data = await response.json();
-      setApiFetch(data);
-    }
-
-    getData();
-
-    console.log(apiFetch);
-
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setApiFetch(data))
+      .catch((error) => console.error(error));
     return () => controller.abort();
   }, []);
-
-  return <></>;
+  return (
+    <>
+      <div className='wrapper'>
+        {apiFetch.map(({id, title, internal_title, url, intro, duration, image, type, has_summative_assessment}) => {
+          return (
+            <div className="path-wrap" key={id}>
+              <Path title={title}></Path>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/path.tsx
@@ -0,0 +1,9 @@
+function Path({ title }: { title: string }) {
+  return (
+    <div className='path-wrap'>
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default Path;
