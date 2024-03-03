f2e69b5bfcb91926a192641636d358cba90a9a95
Anish <anishkakaiya@outlook.com>
2024-03-03 18:19:28 +0000

    add sorting functionality for a-z and most recent according to ID

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,20 @@ function App() {
   const controller = new AbortController();
   const [apiFetch, setApiFetch] = useState<Array<PathType>>([]);
 
+  const sortAlphabetical = () => {
+    const sortedData = [...apiFetch].sort((a: PathType, b: PathType) =>
+      a.title.localeCompare(b.title)
+    );
+    setApiFetch(sortedData);
+  };
+
+  const sortRecent = () => {
+    const sortedData = [...apiFetch].sort(
+      (a: PathType, b: PathType) => b.id - a.id
+    );
+    setApiFetch(sortedData);
+  };
+
   useEffect(() => {
     fetch(URL)
       .then((response) => response.json())
@@ -19,6 +33,13 @@ function App() {
   return (
     <main>
       <div className='wrapper'>
+        <div className='sort-wrapper'>
+          <h3>Sort</h3>
+          <div className='sort-options'>
+            <button onClick={sortAlphabetical}>A-Z</button>
+            <button onClick={sortRecent}>Most Recent</button>
+          </div>
+        </div>
         <ul>
           <li>
             {apiFetch.map(
