6b2c1d85a9f9e4e116d10b95b8e20678b4802193
Anish <anishkakaiya@outlook.com>
2024-03-03 17:20:33 +0000

    write markup for path component

--- frontend/src/path.tsx
+++ frontend/src/path.tsx
@@ -1,12 +1,29 @@
 import type PathType from '../lib/constants';
 import './path.scss';
 
-function Path({ title, image, url }: PathType) {
+function Path({
+  title,
+  image,
+  url,
+  internal_title,
+  intro,
+  duration,
+  type,
+  has_summative_assessment,
+}: PathType) {
   return (
     <div className='path-wrap-inner'>
-      <img src={image} alt='' />
-      <h2>{title}</h2>
-      <a href={url}>View Pathway →</a>
+      <img src={image} alt={`image of ${title} to represent course`} />
+      <div className='text-wrap'>
+        <h2 className='course headline'>{title}</h2>
+        <p className='course subline'>
+          {type} • {duration}
+        </p>
+        {intro.length ? <h3 className='course intro-text'>{intro}</h3> : null}
+        <button>
+          <a href={url}>View Pathway →</a>
+        </button>
+      </div>
     </div>
   );
 }
