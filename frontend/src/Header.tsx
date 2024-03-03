2679df53d417bc191c6950a8fba3942bb0a93c5f
Anish <anishkakaiya@outlook.com>
2024-03-03 18:51:27 +0000

    add header

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,17 @@
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <a href='https://www.dermatica.co.uk/'>
+        <img
+          src='https://www.blackbullion.com/assets/images/logos/blackbullion-full-lilac-asterisk.svg'
+          alt='company logo'
+          height={40}
+        />
+      </a>
+    </header>
+  );
+};
+
+export default Header;
